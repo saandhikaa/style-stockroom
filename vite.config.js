ecd887e31fbe2839af16b47c104301d24ec67573
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-11-12 22:30:02 +0700

    setup tailwind css

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { resolve } from 'path'; // Add this line to import the path module
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  server: {
+    watch: {
+      // Exclude unnecessary directories from being watched
+      // Replace './node_modules' and './vendor' with the directories you want to exclude
+      ignored: [resolve(__dirname, './node_modules'), resolve(__dirname, './vendor')]
+    }
+  }
 });
+ 
