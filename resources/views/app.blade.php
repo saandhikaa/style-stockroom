19e3ce5fe2b2daf43930ff55d796df9cbd2fb33f
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-11-12 22:37:15 +0700

    test: create view using tailwindcss

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold text-red-500">Hello world!</h1>
+</body>
+</html>
