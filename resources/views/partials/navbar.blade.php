317daf6f25bb12d1aba651b617392731ab3cef09
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-11-19 17:36:32 +0700

    add product to navigation menu

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,6 +2,7 @@
     <h1 class="text-2xl mb-2">STYLE STOCKROOM</h1>
     <ul class="flex space-x-4">
         <li><a href="/" class="hover:underline">Home</a></li>
+        <li><a href="/product" class="hover:underline">Product</a></li>
         <li><a href="/contact" class="hover:underline">Contact</a></li>
         <li><a href="/about" class="hover:underline">About</a></li>
     </ul>
