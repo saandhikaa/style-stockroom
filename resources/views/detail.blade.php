85eead35b3f17e860b1e4452263af50b988ffba9
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-11-22 14:11:34 +0700

    create product detail page

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('main')
+    <h1 class="text-2xl font-semibold mb-2">{{ $product["name"] }}</h1>
+    <p class="text-lg font-semibold">{{ $product["price"] }}</p>
+    <p>{{ $product["sizes"] }}</p>
+    <p>{{ $product["colors"] }}</p>
+    <p class="italic mb-4">"{{ $product["description"] }}"</p>
+    <a href="/product" class="text-blue-600">back</a>
+@endsection
