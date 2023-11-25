a34ce4175240efb70e27dfb951bec668d224efe7
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-11-25 17:17:27 +0700

    add column: name, price, sizes, colors, description, published_at

--- database/migrations/2023_11_25_042608_create_products_table.php
+++ database/migrations/2023_11_25_042608_create_products_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->string('sizes');
+            $table->string('colors');
+            $table->text('description');
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
