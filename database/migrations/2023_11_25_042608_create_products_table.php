8495b91e80bd5954a79e9daf40bdf498647dff1a
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-11-26 14:26:47 +0700

    add column: slug

--- database/migrations/2023_11_25_042608_create_products_table.php
+++ database/migrations/2023_11_25_042608_create_products_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug')->unique();
             $table->integer('price');
             $table->string('sizes');
             $table->string('colors');
