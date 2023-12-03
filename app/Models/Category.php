a0a253c0ce564420408361e1dd9819121c3b6613
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-12-03 13:24:05 +0700

    update model: Category: add public function product

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,4 +10,9 @@ class Category extends Model
     use HasFactory;
     
     protected $guarded = ['id'];
+    
+    public function product()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
