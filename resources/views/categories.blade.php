b90be4c0f5a429cae0860e19cbc32b14d44d3857
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-12-03 14:10:31 +0700

    create page: categories

---
+++ resources/views/categories.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('main')
+    <h1 class="text-2xl font-semibold mb-8 text-center">All Category</h1>
+    <ul>
+        @foreach ($categories as $category)
+        <li class="mb-2"><a href="/categories/{{ $category->slug }}" class="text-blue-500">{{ $category->name }}</a></li>
+        @endforeach
+    </ul>
+@endsection
