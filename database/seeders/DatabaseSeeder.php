664da04c2501f22742a4ac11c49c0678692c9d3d
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-12-04 15:07:19 +0700

    create user using factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        User::factory(2)->create();
         
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
