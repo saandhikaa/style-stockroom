5515e29387c2416ad171595121993be332c42c3b
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-12-22 16:17:59 +0700

    update ProductController: index: update title, add category

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Category;
 
 class ProductController extends Controller {
     public function index() {
+        $title = '';
+        if (request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
+        }
         
         return view('product', [
-            'title' => 'All Product',
+            'title' => 'All Product' . $title,
             'products' => Product::latest()->filter(request(['search', 'category']))->get()
         ]);
     }
