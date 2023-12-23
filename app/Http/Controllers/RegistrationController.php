9ee143f3ffaa978e73dfd4235735cf05ffece452
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-12-23 14:51:43 +0700

    create RegistrationController

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegistrationController extends Controller
+{
+    public function index()
+    {
+        return view('registration.index', [
+            'title' => 'Registration'
+        ]);
+    }
+}
