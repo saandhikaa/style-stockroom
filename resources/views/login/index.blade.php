38ccd154038cec308bf3bdd7355d84e9d59d8baa
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-12-26 14:12:47 +0700

    update page: login.index: change field username to email

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -8,10 +8,10 @@
                 <form action="/login" method="post">
                     @csrf
                     <div class="mb-5">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                            Username
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                            Email
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" name="username">
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" name="email">
                     </div>
                     <div class="mb-5">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
