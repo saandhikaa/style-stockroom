59f4003c5f84211c492261c6e06759a88ebc763d
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-12-26 14:47:23 +0700

    update page: registration.index: show error

--- resources/views/registration/index.blade.php
+++ resources/views/registration/index.blade.php
@@ -11,19 +11,28 @@
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
                             Name
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Name" name="name">
+                        <input class="shadow appearance-none border {{ $errors->has('name') ? 'border-red-500' : '' }} rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Name" name="name">
+                        @error('name')
+                            <span class="text-red-500 text-xs italic">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-5">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                             Email
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" name="email">
+                        <input class="shadow appearance-none border {{ $errors->has('email') ? 'border-red-500' : '' }} rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" name="email">
+                        @error('email')
+                            <span class="text-red-500 text-xs italic">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-5">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                             Password
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" name="password">
+                        <input class="shadow appearance-none border {{ $errors->has('password') ? 'border-red-500' : '' }} rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" name="password">
+                        @error('password')
+                            <span class="text-red-500 text-xs italic">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="flex items-center justify-between">
                         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
