6f2f4945145d4fc88aaa078f6d01bf123556bc60
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-12-26 14:11:57 +0700

    update page: registration.index: remove username field

--- resources/views/registration/index.blade.php
+++ resources/views/registration/index.blade.php
@@ -14,12 +14,6 @@
                         <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Name" name="name">
                     </div>
                     <div class="mb-5">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-                            Username
-                        </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" name="username">
-                    </div>
-                    <div class="mb-5">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                             Email
                         </label>
