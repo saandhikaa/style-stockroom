5e0b4be6cd656d4979d51657b72f837fca1b2606
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-12-31 18:23:02 +0700

    update LoginController: method authenticate: add auth

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -15,11 +16,16 @@ class LoginController extends Controller
     
     public function authenticate (Request $request)
     {
-        $request->validate([
+        $credentials = $request->validate([
             'email' => 'required|email:dns',
             'password' => 'required'
         ]);
         
-        dd($request->all());
+        if(Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+        
+        return back()->with('authFail', 'Email/Password is incorrect.');
     }
 }
