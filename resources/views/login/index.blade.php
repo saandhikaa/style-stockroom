3b35829d66805080b3a8024fbc260ba51f7e2e69
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-12-31 17:47:40 +0700

    update page: login.index: show error

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -11,7 +11,7 @@
             </button>
         </div>
     @endif
-
+    
     <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
         <div class="md:flex">
             <div class="p-8">
@@ -22,13 +22,19 @@
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                             Email
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" name="email">
+                        <input class="shadow appearance-none border {{ $errors->has('email') ? 'border-red-500' : '' }} rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" name="email" required>
+                        @error('email')
+                            <span class="text-red-500 text-xs italic">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="mb-5">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
                             Password
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" name="password">
+                        <input class="shadow appearance-none border {{ $errors->has('password') ? 'border-red-500' : '' }} rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" name="password" required>
+                        @error('password')
+                            <span class="text-red-500 text-xs italic">{{ $message }}</span>
+                        @enderror
                     </div>
                     <div class="flex items-center justify-between">
                         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
