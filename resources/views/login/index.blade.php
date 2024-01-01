261a68f68be65288f44ba97791f2c2d51d6c6b74
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-01-01 13:25:09 +0700

    update page: login.index: add flash message for invalid email or password

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -12,6 +12,17 @@
         </div>
     @endif
     
+    @if (session()->has('authFail'))
+        <div class="bg-red-500 text-white px-6 py-4 border-0 rounded relative mb-6">
+            <span class="inline-block align-middle mr-8">
+                {{ session()->get('authFail') }}
+            </span>
+            <button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none" onclick="this.parentElement.remove();">
+                <span>×</span>
+            </button>
+        </div>
+    @endif
+    
     <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
         <div class="md:flex">
             <div class="p-8">
