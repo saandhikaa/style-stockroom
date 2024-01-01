12e17e11de4c1fc329069e4d2acf20f274f53cdc
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-01-01 14:23:22 +0700

    update partials: navbar: remove home & move login to right

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,9 +1,10 @@
 <div class="bg-blue-500 p-4 text-white">
-    <h1 class="text-2xl mb-2">STYLE STOCKROOM</h1>
-    <ul class="flex space-x-4">
-        <li><a href="/" class="hover:underline">Home</a></li>
-        <li><a href="/products" class="hover:underline">Product</a></li>
-        <li><a href="/categories" class="hover:underline">Category</a></li>
+    <h1 class="text-2xl mb-2"><a href="/" class="hover:underline">STYLE STOCKROOM</a></h1>
+    <ul class="flex justify-between space-x-4">
+        <div class="flex space-x-4">
+            <li><a href="/products" class="hover:underline">Product</a></li>
+            <li><a href="/categories" class="hover:underline">Category</a></li>
+        </div>
         <li><a href="/login" class="hover:underline">Login</a></li>
     </ul>
 </div>
