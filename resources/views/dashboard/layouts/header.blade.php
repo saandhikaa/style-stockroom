ad3f28ac1479dae73694c3380b22c00b97102cc0
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-01-09 16:12:15 +0700

    create layout: dashboard.layout.header

---
+++ resources/views/dashboard/layouts/header.blade.php
@@ -0,0 +1,6 @@
+<div class="flex items-center justify-center h-14 px-6 bg-blue-500 relative">
+    <button id="openSidebar" class="text-gray-200 hover:text-gray-400 absolute left-4">
+        <span id="openIcon">☰</span>
+    </button>
+    <h1 class="text-2xl text-white">STYLE STOCKROOM</h1>
+</div>
