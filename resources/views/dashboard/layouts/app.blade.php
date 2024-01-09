d78bd6211dc09eb32f654984eaf7152ee7fb56fd
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-01-09 16:11:44 +0700

    create layout: dashboard.layout.app

---
+++ resources/views/dashboard/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard</title>
+
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    @include('dashboard.layouts.header')
+    
+    <main>
+        @yield('main')
+    </main>
+    
+    @include('dashboard.layouts.sidebar')
+</body>
+</html>
