1b810a6656e418e1daddd03b8aa0ef30f0c65808
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-01-10 15:56:47 +0700

    update page: dashboard.product.index: show table list of products

--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -1,5 +1,29 @@
 @extends('dashboard.layouts.app')
 
 @section('main')
-    <h1>Welcome to Dashboard Product, adel!</h1>
+    <h1 class="text-2xl mt-4">Product list</h1>
+    <div class="overflow-x-auto mt-4">
+        <table class="table-auto w-full">
+            <thead>
+                <tr>
+                    <th class="px-4 py-2"></th>
+                    <th class="px-4 py-2">Name</th>
+                    <th class="px-4 py-2">Sizes</th>
+                    <th class="px-4 py-2">Colors</th>
+                    <th class="px-4 py-2">Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($products as $product)
+                    <tr>
+                        <td class="border px-4 py-2">{{ $loop->iteration }}</td>
+                        <td class="border px-4 py-2">{{ $product->name }}</td>
+                        <td class="border px-4 py-2">{{ $product->sizes }}</td>
+                        <td class="border px-4 py-2">{{ $product->colors }}</td>
+                        <td class="border px-4 py-2">{{ $product->price }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
