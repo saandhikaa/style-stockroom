b4acbf8bdc7c67c8f1722d43d95548863fb56268
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-01-11 16:14:35 +0700

    update page: dashboard.product.index: add column action

--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -11,6 +11,7 @@
                     <th class="px-4 py-2">Sizes</th>
                     <th class="px-4 py-2">Colors</th>
                     <th class="px-4 py-2">Price</th>
+                    <th class="px-4 py-2">Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -21,6 +22,13 @@
                         <td class="border px-4 py-2">{{ $product->sizes }}</td>
                         <td class="border px-4 py-2">{{ $product->colors }}</td>
                         <td class="border px-4 py-2">Rp. {{ number_format($product->price, 0, ',', '.') }}</td>
+                        <td class="border px-4 py-2">
+                            <ul>
+                                <li><a href="/dashboard/products/{{ $product->id }}" class="inline-block bg-blue-500 text-white text-xs px-2 py-1 rounded-full">view</a></li>
+                                <li><a href="" class="inline-block bg-green-500 text-white text-xs px-2 py-1 rounded-full">edit</a></li>
+                                <li><a href="" class="inline-block bg-red-500 text-white text-xs px-2 py-1 rounded-full">delete</a></li>
+                            </ul>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
