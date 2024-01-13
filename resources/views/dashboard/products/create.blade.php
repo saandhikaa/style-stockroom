40eb9f24651f8fb6ecd19d649f5bc7dcba366993
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-01-13 21:49:52 +0700

    update page: dashboard.product.create: add field price in form

--- resources/views/dashboard/products/create.blade.php
+++ resources/views/dashboard/products/create.blade.php
@@ -46,6 +46,11 @@
             </div>
         </div>
         
+        <div class="items-center mb-6">
+            <label class="block text-gray-500 font-bold mb-1 pr-4" for="price">Price</label>
+            <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="price" name="price" type="number" placeholder="Product price">
+        </div>
+        
         <div class="items-center">
             <button class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">Add Product</button>
         </div>
