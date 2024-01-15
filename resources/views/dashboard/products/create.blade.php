052f8728c13fbf9e5d21955cc881300138b5172a
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-01-15 17:44:50 +0700

    update page: dashboard.product.create: add previous value on colors

--- resources/views/dashboard/products/create.blade.php
+++ resources/views/dashboard/products/create.blade.php
@@ -51,10 +51,12 @@
         <div class="items-center mb-6">
             <label class="block text-gray-500 font-bold mb-1 pr-4">Colors</label>
             <div class="colors-container flex flex-wrap items-center">
-                <div class="flex items-center mr-2 mb-2">
-                    <input type="text" name="colors[]" placeholder="One color" class="dynamic-width-input bg-gray-200 appearance-none border-2 border-gray-200 rounded-l-md h-10 w-24 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 text-center">
-                    <button type="button" class="delete-color-button bg-gray-200 hover:bg-red-400 font-bold text-2xl text-gray-700 h-10 px-3 rounded-r-md border-l-2 border-l-gray-300">×</button>
-                </div>
+                @foreach(old('colors', ['']) as $color)
+                    <div class="flex items-center mr-2 mb-2">
+                        <input type="text" name="colors[]" placeholder="One color" class="dynamic-width-input bg-gray-200 appearance-none border-2 border-gray-200 rounded-l-md h-10 w-24 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 text-center" value="{{ $color }}">
+                        <button type="button" class="delete-color-button bg-gray-200 hover:bg-red-400 font-bold text-2xl text-gray-700 h-10 px-3 rounded-r-md border-l-2 border-l-gray-300">×</button>
+                    </div>
+                @endforeach
                 <button type="button" class="add-color-button shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white text-2xl h-10 w-10 mb-2 rounded">+</button>
             </div>
         </div>
