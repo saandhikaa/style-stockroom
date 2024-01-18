a81fab519f10a26499c6209c42366d22352b8a20
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-01-18 16:35:28 +0700

    update page: dashboard.product.index: update flash message

--- resources/views/dashboard/products/index.blade.php
+++ resources/views/dashboard/products/index.blade.php
@@ -3,10 +3,10 @@
 @section('main')
     <h1 class="text-2xl mt-4">Product list</h1>
     
-    @if (session()->has('added'))
-        <div class="bg-blue-500 text-white px-6 py-4 border-0 rounded relative my-6">
+    @if (session()->has('success'))
+        <div class="bg-green-200 text-green-800 border-green-300 border-2 px-6 py-4 rounded-md relative my-6">
             <span class="inline-block align-middle mr-8">
-                {{ session()->get('added') }}
+                {{ session()->get('success') }}
             </span>
             <button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none" onclick="this.parentElement.remove();">
                 <span>×</span>
