6a90122bbe9f3c787d328388442249cdd3365ac1
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-01-25 20:11:37 +0700

    update page: dashboard.product.show: update edit button

--- resources/views/dashboard/products/show.blade.php
+++ resources/views/dashboard/products/show.blade.php
@@ -4,7 +4,7 @@
     <h1 class="text-2xl font-semibold mb-2">{{ $product->name }}</h1>
     <ul class="flex space-x-2">
         <li><a href="/dashboard/products" class="inline-block bg-blue-500 text-white text-xs px-2 py-1 rounded-md">Back to All Products</a></li>
-        <li><a href="" class="inline-block bg-green-500 text-white text-xs px-2 py-1 rounded-md">Edit</a></li>
+        <li><a href="/dashboard/products/{{ $product->slug }}/edit" class="inline-block bg-green-500 text-white text-xs px-2 py-1 rounded-md">Edit</a></li>
         <li>
             <form method="POST" action="/dashboard/products/{{ $product->slug }}" onsubmit="return confirm('Are you sure you want to delete {{ $product->name }}?');">
                 @csrf
