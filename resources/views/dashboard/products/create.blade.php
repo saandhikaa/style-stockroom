95bdd82195463f6d15816c7b1d48b814db020703
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-01-25 21:54:06 +0700

    update page: dashboard.product.create: add error handling for image upload field

--- resources/views/dashboard/products/create.blade.php
+++ resources/views/dashboard/products/create.blade.php
@@ -24,9 +24,12 @@
         
         <div class="items-center mb-6">
             <label class="block text-gray-500 font-bold mb-1 pr-4" for="image">Image</label>
-            <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="image" name="image" type="file">
+            <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 {{ $errors->has('image') ? 'border-red-500' : '' }}" id="image" name="image" type="file" required>
+            @error('image')
+                <span class="text-red-500 text-xs italic">{{ $message }}</span>
+            @enderror
         </div>
-
+        
         <div class="items-center mb-6">
             <label class="block text-gray-500 font-bold mb-1 pr-4" for="category">Category</label>
             <select class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="category" name="category_id">
