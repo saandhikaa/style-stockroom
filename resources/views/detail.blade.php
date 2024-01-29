1c13db89772b8aab5b506d17c157c3247017b60c
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-01-29 19:44:20 +0700

    update page: detail: add image & set price in idr

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -2,7 +2,12 @@
 
 @section('main')
     <h1 class="text-2xl font-semibold mb-2">{{ $product->name }}</h1>
-    <p class="text-lg font-semibold">{{ $product->price }}</p>
+    @if ($product->image)
+        <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" class="rounded-xl border-2 border-blue-500">
+    @else
+        <img src="https://source.unsplash.com/1000x1000/?{{ $product->category->name }}" alt="{{ $product->name }}" class="rounded-xl border-2 border-blue-500">
+    @endif
+    <p class="text-lg font-semibold">Rp. {{ number_format($product->price, 0, ',', '.') }}</p>
     <p>{{ $product->sizes }}</p>
     <p>{{ $product->colors }}</p>
     <p class="italic mb-4">"{!! $product->description !!}"</p>
