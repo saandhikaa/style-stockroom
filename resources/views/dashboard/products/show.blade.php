e3fd2f0609e690b74f9891125537fc2d5ed40500
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-01-29 19:36:47 +0700

    update page: dashboard.product.show: add some style

--- resources/views/dashboard/products/show.blade.php
+++ resources/views/dashboard/products/show.blade.php
@@ -1,8 +1,8 @@
 @extends('dashboard.layouts.app')
 
 @section('main')
-    <h1 class="text-2xl font-semibold mb-2">{{ $product->name }}</h1>
-    <ul class="flex space-x-2">
+    <h1 class="text-3xl font-semibold mb-2">{{ $product->name }}</h1>
+    <ul class="flex space-x-2 mb-4">
         <li><a href="/dashboard/products" class="inline-block bg-blue-500 text-white text-xs px-2 py-1 rounded-md">Back to All Products</a></li>
         <li><a href="/dashboard/products/{{ $product->slug }}/edit" class="inline-block bg-green-500 text-white text-xs px-2 py-1 rounded-md">Edit</a></li>
         <li>
@@ -17,12 +17,12 @@
     </ul>
     
     @if ($product->image)
-        <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}">
+        <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" class="rounded-xl border-2 border-blue-500">
     @else
-        <img src="https://source.unsplash.com/1000x1000/?{{ $product->category->name }}" alt="{{ $product->name }}">
+        <img src="https://source.unsplash.com/1000x1000/?{{ $product->category->name }}" alt="{{ $product->name }}" class="rounded-xl border-2 border-blue-500">
     @endif
-    <p class="text-lg font-semibold">{{ $product->price }}</p>
+    <p class="text-lg font-semibold mt-4">{{ $product->price }}</p>
     <p>{{ $product->sizes }}</p>
     <p>{{ $product->colors }}</p>
-    <p class="italic mb-4">"{!! $product->description !!}"</p>
+    <p class="italic my-4">"{!! $product->description !!}"</p>
 @endsection
