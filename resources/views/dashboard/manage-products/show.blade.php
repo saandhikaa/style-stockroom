d777b0c7feed5ea401ca93e2c0e5b5541a9b2b61
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-02-09 10:15:32 +0700

    update views: dashboard.manage-products.show: fix url path

--- resources/views/dashboard/manage-products/show.blade.php
+++ resources/views/dashboard/manage-products/show.blade.php
@@ -1,12 +1,12 @@
-@extends('dashboard.layouts.app')
+@extends('layouts.dashboard')
 
 @section('main')
     <h1 class="text-3xl font-semibold mb-2">{{ $product->name }}</h1>
     <ul class="flex space-x-2 mb-4">
-        <li><a href="/dashboard/products" class="inline-block bg-blue-500 text-white text-xs px-2 py-1 rounded-md">Back to All Products</a></li>
-        <li><a href="/dashboard/products/{{ $product->slug }}/edit" class="inline-block bg-green-500 text-white text-xs px-2 py-1 rounded-md">Edit</a></li>
+        <li><a href="/dashboard/manage-products" class="inline-block bg-blue-500 text-white text-xs px-2 py-1 rounded-md">Back to All Products</a></li>
+        <li><a href="/dashboard/manage-products/{{ $product->slug }}/edit" class="inline-block bg-green-500 text-white text-xs px-2 py-1 rounded-md">Edit</a></li>
         <li>
-            <form method="POST" action="/dashboard/products/{{ $product->slug }}" onsubmit="return confirm('Are you sure you want to delete {{ $product->name }}?');">
+            <form method="POST" action="/dashboard/manage-products/{{ $product->slug }}" onsubmit="return confirm('Are you sure you want to delete {{ $product->name }}?');">
                 @csrf
                 @method('DELETE')
                 <button type="submit" class="inline-block bg-red-500 text-white text-xs px-2 py-1 rounded-md">
