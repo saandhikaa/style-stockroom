787f057ad42a0ff82778f6622f8dae987760903d
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-02-09 15:49:19 +0700

    update model User: add relationship to carts

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
 }
