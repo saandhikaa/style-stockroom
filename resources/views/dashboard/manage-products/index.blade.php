f9f4d55241a9bab727110fb08d35843230da34c4
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-02-09 10:14:03 +0700

    update views: dashboard.manage-products.index: fix url path

--- resources/views/dashboard/manage-products/index.blade.php
+++ resources/views/dashboard/manage-products/index.blade.php
@@ -1,4 +1,4 @@
-@extends('dashboard.layouts.app')
+@extends('layouts.dashboard')
 
 @section('main')
     <h1 class="text-2xl mt-4">Product list</h1>
@@ -14,7 +14,7 @@
         </div>
     @endif
     
-    <a href="/dashboard/products/create" class="inline-block bg-blue-500 text-white mt-4 px-4 py-1 rounded-md">Add new product</a>
+    <a href="/dashboard/manage-products/create" class="inline-block bg-blue-500 text-white mt-4 px-4 py-1 rounded-md">Add new product</a>
     
     <div class="overflow-x-auto mt-4">
         <table class="table-auto w-full">
@@ -38,10 +38,10 @@
                         <td class="border px-4 py-2">Rp. {{ number_format($product->price, 0, ',', '.') }}</td>
                         <td class="border px-4 py-2">
                             <ul>
-                                <li><a href="/dashboard/products/{{ $product->slug }}" class="inline-block bg-blue-500 text-white text-xs px-2 py-1 rounded-full">view</a></li>
-                                <li><a href="/dashboard/products/{{ $product->slug }}/edit" class="inline-block bg-green-500 text-white text-xs px-2 py-1 rounded-full">edit</a></li>
+                                <li><a href="/dashboard/manage-products/{{ $product->slug }}" class="inline-block bg-blue-500 text-white text-xs px-2 py-1 rounded-full">view</a></li>
+                                <li><a href="/dashboard/manage-products/{{ $product->slug }}/edit" class="inline-block bg-green-500 text-white text-xs px-2 py-1 rounded-full">edit</a></li>
                                 <li>
-                                    <form method="POST" action="/dashboard/products/{{ $product->slug }}" onsubmit="return confirm('Are you sure you want to delete {{ $product->name }}?');">
+                                    <form method="POST" action="/dashboard/manage-products/{{ $product->slug }}" onsubmit="return confirm('Are you sure you want to delete {{ $product->name }}?');">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" class="inline-block bg-red-500 text-white text-xs px-2 py-1 rounded-full">
