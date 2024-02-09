c05aa2d73f7ca570a298e86b87360600e6354da5
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-02-09 14:57:35 +0700

    update table carts: add columns user_id, product_id & quantity

--- database/migrations/2024_02_09_073700_create_carts_table.php
+++ database/migrations/2024_02_09_073700_create_carts_table.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         Schema::dropIfExists('carts');
