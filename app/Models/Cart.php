c476ebb9e809459b675aee1d60db63be632b7fb7
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-02-09 15:48:24 +0700

    update model Cart: add guarded, add relationship to user and product

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+    
+    protected $guarded = ['id'];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
