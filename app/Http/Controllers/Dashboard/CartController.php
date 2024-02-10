80244095a8b77c42f5e5c4e18b1b80dac601368e
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-02-10 11:27:22 +0700

    update controller: CartController: create method: add

--- app/Http/Controllers/Dashboard/CartController.php
+++ app/Http/Controllers/Dashboard/CartController.php
@@ -4,8 +4,21 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Cart;
+use App\Models\Product;
+use Auth;
 
 class CartController extends Controller
 {
-    //
+    public function add (Request $request, Product $product)
+    {
+        $cart = new Cart;
+        $cart->user_id = Auth::id();
+        $cart->product_id = $product->id;
+        $cart->quantity = $request->quantity;
+        
+        $cart->save();
+        
+        return back()->with('success', "Product added to cart successfully!");
+    }
 }
