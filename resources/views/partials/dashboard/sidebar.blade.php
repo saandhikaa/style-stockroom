d5683cf1266d9bb19c8f623639d3bf060d6ae0e1
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-02-10 11:58:03 +0700

    update views: partials.dashboard.sidebar: add link: shopping cart

--- resources/views/partials/dashboard/sidebar.blade.php
+++ resources/views/partials/dashboard/sidebar.blade.php
@@ -7,6 +7,7 @@
     </div>
     <ul class="mt-4 space-y-1 flex-grow">
         <li><a href="/dashboard" class="px-4 py-2 text-left hover:bg-blue-500 hover:text-white w-full block {{ Request::is('dashboard') ? 'font-semibold text-blue-500' : 'text-gray-800' }}">Dashboard</a></li>
+        <li><a href="/dashboard/cart" class="px-4 py-2 text-left hover:bg-blue-500 hover:text-white w-full block {{ Request::is('dashboard/cart') ? 'font-semibold text-blue-500' : 'text-gray-800' }}">Shopping Cart</a></li>
         
         @can('admin')
             <li><a href="/dashboard/manage-products" class="px-4 py-2 text-left hover:bg-blue-500 hover:text-white w-full block {{ Request::is('dashboard/manage-products*') ? 'font-semibold text-blue-500' : 'text-gray-800' }} ">Manage Product</a></li>
