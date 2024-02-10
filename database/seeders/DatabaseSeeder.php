4955ed24d4b0b8bd5bf9a7369d30a02b9fee890a
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-02-10 12:30:05 +0700

    update DatabaseSeeder: create new user & add stock to all product

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,13 @@ class DatabaseSeeder extends Seeder
             "level" => "administrator"
         ]);
         
-        User::factory(2)->create();
+        User::create([
+            "name" => "Azizi Shafa",
+            "email" => "zee@jkt.com",
+            "password" => bcrypt("123456")
+        ]);
+        
+        User::factory(1)->create();
         
         Category::create([
             "name" => "Outerwear",
@@ -58,6 +64,7 @@ class DatabaseSeeder extends Seeder
             "slug" => "vintage-leather-jacket",
             "category_id" => 1,
             "price" => 2250000,
+            "stock" => 10,
             "description" => "A stylish vintage leather jacket for a timeless look.",
             "sizes" => "S, M, L, XL",
             "colors" => "Black, Brown"
@@ -68,6 +75,7 @@ class DatabaseSeeder extends Seeder
             "slug" => "slim-fit-jeans",
             "category_id" => 2,
             "price" => 1200000,
+            "stock" => 10,
             "description" => "Comfortable and fashionable slim fit jeans.",
             "sizes" => "28, 30, 32, 34, 36",
             "colors" => "Blue, Black, Grey"
@@ -78,6 +86,7 @@ class DatabaseSeeder extends Seeder
             "slug" => "classic-denim-jacket",
             "category_id" => 1,
             "price" => 1250000,
+            "stock" => 10,
             "description" => "A classic denim jacket for a casual look.",
             "sizes" => "S, M, L, XL",
             "colors" => "Blue, Black"
@@ -88,6 +97,7 @@ class DatabaseSeeder extends Seeder
             "slug" => "silk-scarf",
             "category_id" => 3,
             "price" => 675000,
+            "stock" => 10,
             "description" => "Elegant silk scarf with a beautiful pattern.",
             "sizes" => "One Size",
             "colors" => "Red, Blue, Green, Yellow"
@@ -98,6 +108,7 @@ class DatabaseSeeder extends Seeder
             "slug" => "wool-scarf",
             "category_id" => 3,
             "price" => 500000,
+            "stock" => 10,
             "description" => "Warm wool scarf for the cold weather.",
             "sizes" => "One Size",
             "colors" => "Red, Blue, Green, Yellow"
@@ -108,6 +119,7 @@ class DatabaseSeeder extends Seeder
             "slug" => "high-waist-jeans",
             "category_id" => 2,
             "price" => 950000,
+            "stock" => 10,
             "description" => "Stylish high waist jeans for a trendy look.",
             "sizes" => "28, 30, 32, 34, 36",
             "colors" => "Blue, Black, Grey"
@@ -118,6 +130,7 @@ class DatabaseSeeder extends Seeder
             "slug" => "leather-boots",
             "category_id" => 5,
             "price" => 1900000,
+            "stock" => 10,
             "description" => "Stylish and durable leather boots.",
             "sizes" => "38, 39, 40, 41, 42",
             "colors" => "Black, Brown"
@@ -128,6 +141,7 @@ class DatabaseSeeder extends Seeder
             "slug" => "floral-summer-dress",
             "category_id" => 4,
             "price" => 850000,
+            "stock" => 10,
             "description" => "Light and breezy summer dress with a beautiful floral pattern.",
             "sizes" => "S, M, L, XL",
             "colors" => "Red, Blue, Yellow"
@@ -138,6 +152,7 @@ class DatabaseSeeder extends Seeder
             "slug" => "canvas-belt",
             "category_id" => 3,
             "price" => 150000,
+            "stock" => 10,
             "description" => "Versatile canvas belt.",
             "sizes" => "One Size",
             "colors" => "Black, Brown, Blue"
@@ -148,6 +163,7 @@ class DatabaseSeeder extends Seeder
             "slug" => "cotton-t-shirt",
             "category_id" => 6,
             "price" => 200000,
+            "stock" => 10,
             "description" => "Comfortable cotton t-shirt for everyday wear.",
             "sizes" => "S, M, L, XL",
             "colors" => "White, Black, Grey"
